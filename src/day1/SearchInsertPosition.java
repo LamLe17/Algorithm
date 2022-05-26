dd137083bba7cd88ed9cf24ff2684a1683815eb4
lamlh <lehonglamht3@gmail.com>
2022-05-26 23:18:01 +0700

    Initial commit

---
+++ src/day1/SearchInsertPosition.java
@@ -0,0 +1,27 @@
+package day1;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int [] nums = new int[]{ 1 ,3 , 6, 7, 9,  11, 21 };
+        int target = 5;
+        int result = searchInsert(nums, target);
+    }
+    public static int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int lower = 0;
+        int higher = n - 1;
+        int index;
+        while (higher >= lower) {
+            index = (higher + lower) / 2;
+            if (nums[index] == target)
+                return index;
+            else if (target > nums[index]) {
+                lower = index + 1;
+            } else
+                higher = index - 1;
+        }
+
+        return lower;
+
+    }
+}
