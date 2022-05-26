fbab2b08aa46006889a3904f2860ba75041cdf56
lamlh <lehonglamht3@gmail.com>
2022-05-26 23:26:07 +0700

    day1 - search insert position

--- src/day1/SearchInsertPosition.java
+++ src/day1/SearchInsertPosition.java
@@ -2,10 +2,12 @@ package day1;
 
 public class SearchInsertPosition {
     public static void main(String[] args) {
+        //Mảng sắp xếp từ bé đến lớn -> đưa ra vị trí của target nếu có, không thì lấy vị trí của  target(lúc chén vào mảng theo thứ tự)
         int [] nums = new int[]{ 1 ,3 , 6, 7, 9,  11, 21 };
         int target = 5;
         int result = searchInsert(nums, target);
     }
+    //So sánh mid của mảng, điều chỉnh để mid gần với target
     public static int searchInsert(int[] nums, int target) {
         int n = nums.length;
         int lower = 0;
